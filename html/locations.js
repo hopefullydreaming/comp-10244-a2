751f6f0108fc616e656b412952964448cc908491
hopeh <hope.hemingway@mohawkcollege.ca>
2025-02-16 22:54:31 -0500

    improved geolocation with marker (not functioning) and 12 premade markers

---
+++ html/locations.js
@@ -0,0 +1,98 @@
+const locations = [
+    {
+        "OBJECTID": 1,
+        "NAME": "Hamilton Museum of Steam & Technology",
+        "ADDRESS": "900 Woodward Ave, Hamilton, ON L8H 7N2",
+        "CATEGORY": "Museum",
+        "LATITUDE": 43.256306,
+        "LONGITUDE": -79.771722
+    },
+    {
+        "OBJECTID": 2,
+        "NAME": "Legends Grill",
+        "ADDRESS": "399 Greenhill Ave, Hamilton, ON L8K 6L6",
+        "CATEGORY": "Restaurant",
+        "LATITUDE": 43.216654, 
+        "LONGITUDE": -79.796772
+    },
+    {
+        "OBJECTID": 3,
+        "NAME": "Landmark Cinemas 6 Jackson Square",
+        "ADDRESS": "2 King St W, Hamilton, ON L8P 1A1",
+        "CATEGORY": "Movie-Theatre",
+        "LATITUDE": 43.257981, 
+        "LONGITUDE": -79.8706930
+    },
+    {
+        "OBJECTID": 4,
+        "NAME": "Hamilton Children's Museum",
+        "ADDRESS": "1072 Main St E, Hamilton, ON L8M 1N6",
+        "CATEGORY": "Museum",
+        "LATITUDE": 43.243337, 
+        "LONGITUDE": -79.826144
+    },
+    {
+        "OBJECTID": 5,
+        "NAME": "Caro",
+        "ADDRESS": "4 Ottawa St N, Hamilton, ON L8H 3Y7",
+        "CATEGORY": "Restaurant",
+        "LATITUDE": 43.242526, 
+        "LONGITUDE": -79.819592
+    },
+    {
+        "OBJECTID": 6,
+        "NAME": "Cineplex Cinemas Hamilton Mountain",
+        "ADDRESS": "795 Paramount Dr, Stoney Creek, ON L8J 0B4",
+        "CATEGORY": "Movie-Theatre",
+        "LATITUDE": 43.193146, 
+        "LONGITUDE": -79.809426
+    },
+    {
+        "OBJECTID": 7,
+        "NAME": "HMCS Haida",
+        "ADDRESS": "Pier 9, 658 Catharine St N, Hamilton, ON L8L 8K4",
+        "CATEGORY": "Museum",
+        "LATITUDE": 43.276762, 
+        "LONGITUDE": -79.854914
+    },
+    {
+        "OBJECTID": 8,
+        "NAME": "One Duke Tavern",
+        "ADDRESS": "1 Duke St, Hamilton, ON L8P 1W9",
+        "CATEGORY": "Restaurant",
+        "LATITUDE": 43.251838, 
+        "LONGITUDE": -79.871942
+    },
+    {
+        "OBJECTID": 9,
+        "NAME": "Starlite Drive-In Theatre",
+        "ADDRESS": "59 Green Mountain Rd E, Stoney Creek, ON L8J 2V5",
+        "CATEGORY": "Movie-Theatre",
+        "LATITUDE": 43.198664, 
+        "LONGITUDE": -79.759043
+    },
+    {
+        "OBJECTID": 10,
+        "NAME": "Battlefield House Museum & Park National Historic Site",
+        "ADDRESS": "77 King St W, Stoney Creek, ON L8G 5E5",
+        "CATEGORY": "Museum",
+        "LATITUDE": 43.218327, 
+        "LONGITUDE": -79.767100
+    },
+    {
+        "OBJECTID": 11,
+        "NAME": "Democracy*",
+        "ADDRESS": "202 Locke St S, Hamilton, ON L8P 4B4",
+        "CATEGORY": "Restaurant",
+        "LATITUDE": 43.254637, 
+        "LONGITUDE": -79.885970
+    },
+    {
+        "OBJECTID": 12,
+        "NAME": "Playhouse Cinema",
+        "ADDRESS": "177 Sherman Ave N, Hamilton, ON L8L 6M8",
+        "CATEGORY": "Movie-Theatre",
+        "LATITUDE": 43.257189, 
+        "LONGITUDE": -79.835825
+    }
+]
